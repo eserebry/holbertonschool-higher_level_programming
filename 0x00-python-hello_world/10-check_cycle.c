82d2ad5e3ec490e6a1657c8b604c1753d87f80e1
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-10 03:45:49 +0000

    first commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "lists.h"
+
+/**
+ *check_cycle - checks if a singly linked list has a cycle in it.
+ *
+ * @head: pointer to a head node of the list
+ *
+ * Return: returns 0 if cycle is found, 1 if it doesn't
+ */
+int check_cycle(listint_t *head)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	slow = head;
+	fast = head;
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			return (0);
+	}
+	return (1);
+}
