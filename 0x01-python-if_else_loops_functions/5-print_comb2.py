74c9e3dd421263ca1fa204d159660087724f2fc1
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-11 02:42:16 +0000

    break statement added

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 i = 0
 for i in range(0, 100):
-    print("{:02d}" .format(i), end=", ")
     if i == 99:
         print("{:d}" .format(i))
+        break
+    print("{:02d}" .format(i), end=", ")
+
