a8f92720f74a182541a1acadbad1f3d1562f1b69
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-11 03:55:17 +0000

    changed for operator to if

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     i = ord(c)
-    for i in range(97, 123):
+    if i in range(97, 123):
         return True
-    for i in range(65, 91):
+    elif i in range(65, 91):
         return False
