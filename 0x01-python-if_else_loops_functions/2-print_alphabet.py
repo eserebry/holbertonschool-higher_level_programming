baf090ecd1bdb265f7b0f65255e37af7ab3a8ff8
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-11 01:51:40 +0000

    added a loop

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-i = 0
-for i in range(97, 123):
-    print(chr(i), end="")
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for a in alpha:
+    print(a, end="")
