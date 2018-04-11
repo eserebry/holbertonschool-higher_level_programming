c981d5afa1a312434927e30ab4302b3640de6502
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-11 23:19:41 +0000

    first commit

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    for i in str:
+        if (n >= 0):
+            new_str = str[:n] + str[n + 1:]
+            return new_str
+        else:
+            return str
