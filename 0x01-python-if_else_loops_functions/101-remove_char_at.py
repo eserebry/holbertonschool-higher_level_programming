9d350956f8042d5468a1195040418265c6ebf2e9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-11 23:33:13 +0000

    delited first loop

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        if n >= 0 and str != "":
-            new_str = str[:n] + str[n + 1:]
-            return new_str
-        else:
-            return str
+    if n >= 0:
+        new_str = str[:n] + str[n + 1:]
+        return new_str
+    else:
+        return str
