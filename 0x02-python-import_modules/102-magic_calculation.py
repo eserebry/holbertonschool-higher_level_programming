0049054e992483bf59ab9f6583e11771494b6f50
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-13 05:27:19 +0000

    return values added

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,5 +5,9 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return c
     else:
-        c = sub(a, b)
+        return sub(a, b)
+
+import dis
+dis.dis(magic_calculation)
