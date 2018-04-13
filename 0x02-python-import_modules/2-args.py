673c77d4a70468fb70ebd96e6f213bf55975485e
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-13 02:48:24 +0000

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+num_arg = len(sys.argv)
+i = 1
+if num_arg == 1:
+    print("0 arguments")
+else:
+    if num_arg == 2:
+        print("{:d}" .format(num_arg - 1), "argument:")
+    elif num_arg > 2:
+        print("{:d}" .format(num_arg - 1), "arguments:")
+    while num_arg != 1:
+        print("{:d}:" .format(i), sys.argv[i])
+        i += 1
+        num_arg -= 1
