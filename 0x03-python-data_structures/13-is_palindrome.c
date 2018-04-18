bca86bdc4e1d66827c76538750f639959d0fd468
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-18 06:53:33 +0000

    first commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - hecks if a singly linked list is a palindrome.
+ * @head: pointer to the adress of head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+}
