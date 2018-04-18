e89733774539d7b85041ffb1bb9169b498f869b8
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-18 17:14:47 +0000

    changed algoritm

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,30 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) >= 2:
-        sum_a = tuple_b[0]
-        sum_b = tuple_b[1]
-    if len(tuple_a) == 0 and len(tuple_b) == 1:
-        sum_a = tuple_b[0]
-        sum_b = 0
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        sum_a = 0
-        sum_b = 0
-    if len(tuple_a) == 1 and len(tuple_b) >= 2:
-        sum_a = tuple_a[0]+tuple_b[0]
-        sum_b = tuple_b[1]
-    if len(tuple_a) == 1 and len(tuple_b) == 1:
-        sum_a = tuple_a[0]+tuple_b[0]
-        sum_b = 0
-    if len(tuple_a) == 1 and len(tuple_b) == 0:
-        sum_a = tuple_a[0]
-        sum_b = 0
-    if len(tuple_b) == 0 and len(tuple_a) >= 2:
-        sum_a = tuple_a[0]
-        sum_b = tuple_a[1]
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        sum_a = tuple_a[0]+tuple_b[0]
-        sum_b = tuple_a[1]+tuple_b[1]
-    if len(tuple_a) >= 2 and len(tuple_b) == 1:
-        sum_a = tuple_a[0]+tuple_b[0]
-        sum_b = tuple_a[1]
-    return sum_a, sum_b
+    while(len(tuple_a) < 2):
+        tuple_a += (0,)
+    while(len(tuple_b) < 2):
+        tuple_b += (0,)
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
