6b444d864089cbf4f6fdebb84adf81c9b0548556
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-18 06:17:28 +0000

    changed case for empty string, added return value

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,8 @@
 def multiple_returns(sentence):
     if sentence == "":
         char = None
+        lenght = 0
+        return lenght, char
     lenght = len(sentence)
     char = sentence[0]
     return lenght, char
