9eff4f1eec545222faac4de296d752477a520af6
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 06:42:59 +0000

    first commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:   
+        best_score = 0
+        name = ''
+        for key, value in a_dictionary.items():
+            if a_dictionary[key] > best_score:
+                name = key
+                best_score = a_dictionary[key]
+        return name
+    return None
