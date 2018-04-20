fc8cb6a5078a9e1315dd6afb139c619eb6f076ed
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 02:49:14 +0000

    deleted built-in enumerate

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    for i, n in enumerate(my_list):
-        if n == search:
+    for i in range(0, (len(new_list))):
+        if new_list[i] == search:
             new_list[i] = replace
     return new_list
