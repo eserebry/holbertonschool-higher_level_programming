2ba86f0e7ad54e6a0ff5436fce41cac523c3db01
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 22:49:33 +0000

    first commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {"I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
+    total = 0
+    if roman_string is not None:
+        prev = 1001
+    for key in roman_string:
+        if dict[key] > prev:
+            total += dict[key] - (prev*2)
+        else:
+            total += dict[key]
+        prev = dict[key]
+    return total
