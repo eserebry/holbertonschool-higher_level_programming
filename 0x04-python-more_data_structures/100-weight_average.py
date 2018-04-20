02ac0b2e47c1a2646702602b2383440018b0bc1a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 23:57:37 +0000

    edge case added

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    top = 0
-    bottom = 0
-    result = 0
-    for i in my_list:
-        top += i[0] * i[1]
-        bottom += i[1]
-    result = top / bottom
-    return result
+    if my_list is not 0:
+        top = 0
+        bottom = 0
+        result = 0
+        for i in my_list:
+            top += i[0] * i[1]
+            bottom += i[1]
+        result = top / bottom
+        return result
+    return None
