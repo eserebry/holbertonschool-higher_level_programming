ad00fbde8fcd8fe2fc9810d064c89a7aa5203fe6
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 06:44:50 +0000

    pep8 corrected

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:   
+    if a_dictionary:
         best_score = 0
         name = ''
         for key, value in a_dictionary.items():
