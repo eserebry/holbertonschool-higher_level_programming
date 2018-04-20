1cc3d265f3e47adf67c59b59495efe8ecdba073a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-20 23:50:16 +0000

    first commit

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/bin/user/python3
+def weight_average(my_list=[]):
+    top = 0
+    bottom = 0
+    result = 0
+    for i in my_list:
+        top += i[0] * i[1]
+        bottom += i[1]
+    result = top / bottom
+    return result
