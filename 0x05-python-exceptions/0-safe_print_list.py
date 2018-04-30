7af0682d04499e86f8c5b00424af439b632f6167
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-30 23:30:17 +0000

    try/except added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
+    real = 0
     for i in my_list:
-        if (i <= x):
-            print("{:d}" .format(i), end="")
-            num += 1
+        try:
+            if (i <= x):
+                print("{:d}" .format(i), end="")
+                real += 1
+        except IndexError:
+            return None
     print(end="\n")
-    return (num)
+    return (real)
