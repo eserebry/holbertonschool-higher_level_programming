d8b3f2dee31976d92858a98ea510538b06f0d23d
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-04-30 23:00:50 +0000

    first commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    num = 0
     for i in my_list:
         if (i <= x):
             print("{:d}" .format(i), end="")
+            num += 1
     print(end="\n")
-    return (x)
+    return (num)
