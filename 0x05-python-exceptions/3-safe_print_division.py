721e7094c030d1b58a08d568fae8b291b2b2dcd2
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-01 01:53:28 +0000

    float type handled

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError):
         return None
     finally:
-        print("Inside result:{:f}".format(c))
+        print("Inside result:{:0.1f}".format(c))
         return c
