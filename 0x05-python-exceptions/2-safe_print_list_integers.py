8244aa50b8af51cfce3d34f3f9092bc3b5a63845
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-01 01:08:44 +0000

    first commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    real = 0
+    my_int_list = [x for x in my_list if isinstance(x, int)]
+    for i in my_int_list:
+        try:
+            if i <= x:
+                print("{:d}" .format(i), end="")
+                real += 1
+        except IndexError:
+            return None
+    print(end="\n")
+    return real
