07d70c5c54e68eca4672c8cf032260bccaa4488a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-05 18:40:02 +0000

    first commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = []
+    if isinstance(matrix, list) is False:
+            raise TypeError("matrix must be a matrix\
+            (list of lists) of integers/floats")
+    for row in matrix:
+        if len(row) == len(matrix[0]):
+            continue
+        else:
+            raise TypeError("Each row of the matrix must have the same size")
+    if isinstance(div, (int, float)) is False:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: round(x/div, 2), i)))
+    return new_matrix
