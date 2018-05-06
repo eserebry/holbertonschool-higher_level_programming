41833b847aae7e3d782c6327a8e2bd6f9263da4a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-06 06:22:13 +0000

    test cases added

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+    My calculation module
+
+"""
+
+
 def print_square(size):
+    """
+        Function that prints a square with the character #.
+
+    """
     if isinstance(size, int) is False:
         raise TypeError("size must be an integer")
     if isinstance(size, float) and size < 0:
