7c0476187406a8a66fe95e4e52a8c73a62861caa
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-06 06:50:29 +0000

    first commit

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+The ``6-max_integer_test`` module
+======================
+
+Using ``max_integer_test``
+-------------------
+
+Importing ``max_integer_test`` from the ``6-max_integer_test`` module:
+
+    >>> max_integer_test = __import__('6-max_integer_test').max_integer_test
+
+Numbers
+=======
+
+Checking for the right output:
+
+ 
+
+Others
+=======
+
+Cheking for the incorrect output:
+
+ 
