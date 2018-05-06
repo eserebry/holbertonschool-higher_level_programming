c08d6fd2967c5f6ee937451804fef4e98979c722
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-06 06:03:39 +0000

    documnetation added

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+    My calculation module
+
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+        Function that prints a square with the character #.
+
+    """
     if isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
     if isinstance(last_name, str) is False:
