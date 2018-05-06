bac4365005b7302015ae9d5d20d85e0a07a42a88
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-06 06:45:58 +0000

    first commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+    My calculation module
+
+"""
+
+
 def text_indentation(text):
+    """
+        Function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+
+    """
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
     for i in range(len(text)):
