ffe22c1af8d08c3cb2925a92c496efa74353cf7e
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-06 05:35:35 +0000

    added test cases as well as edge cases changed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,13 +2,21 @@
 def matrix_divided(matrix, div):
     new_matrix = []
     if isinstance(matrix, list) is False:
-            raise TypeError("matrix must be a matrix\
-            (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix " +
+                            "(list of lists) of integers/floats")
     for row in matrix:
         if len(row) == len(matrix[0]):
             continue
         else:
             raise TypeError("Each row of the matrix must have the same size")
+    for row in matrix:
+        for j in row:
+            if isinstance(j, (int, float)) is False:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) " +
+                    "of integers/floats")
+            else:
+                continue
     if isinstance(div, (int, float)) is False:
         raise TypeError("div must be a number")
     if div == 0:
