b6ee4e4f651d2620fa9a58adad4fa2f1f9f8e12e
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-17 05:20:05 +0000

    first commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Student:
+    """ defines class Student """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation
+        Args:
+            first_name(str): first_name
+            last_name(str): last_name
+            age(int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation of a Student instance """
+        return self.__dict__
