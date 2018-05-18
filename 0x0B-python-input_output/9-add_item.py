ceee212c816703db08788ceb48795058390f28c4
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-18 05:38:31 +0000

    first commit

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-import json
+""" adds all arguments to a Python list, and then save them to a file """
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
+try:
+    args = load_from_json_file(filename)
+except Exception:
+    args = []
 
-def load_from_json_file(filename):
-    """  Write a function that creates an Object from a “JSON file”
-    Args:
-        filename(str): text file
-    """
-    with open(filename, encoding="utf-8") as myFile:
-        return json.load(myFile)
+for i in range(1, len(argv)):
+    args.append(argv[i])
+save_to_json_file(args, filename)
