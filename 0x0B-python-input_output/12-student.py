c05be9c5ba026b2e063217bfca4aa759fca1c003
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-18 05:50:44 +0000

    first commit

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -14,4 +14,12 @@ class Student:
 
     def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a Student instance """
-        return self.__dict__
+
+        if attrs is None:
+            return self.__dict__
+
+        dict = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dict[i] = self.__dict__[i]
+        return dict
