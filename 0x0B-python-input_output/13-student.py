3944992dbb2c14b97be16b6c5320011de078e170
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-18 05:58:56 +0000

    first commit

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -14,4 +14,19 @@ class Student:
 
     def to_json(self, attrs=None):
         """ retrieves a dictionary representation of a Student instance """
-        return self.__dict__
+
+        if attrs is None:
+            return self.__dict__
+
+        dict = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dict[i] = self.__dict__[i]
+        return dict
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+
+        if not json:
+            return
+        self.__dict__ = json
