fec3642ac95d767983c9c86ac228bb17d80ffc6a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-23 04:56:05 +0000

    documentation added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,13 @@ from models.base import Base
 class Rectangle(Base):
     """  class Rectangle that inherits from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Ititialize class Rectangle
+        Args
+            width (int): widht
+            height (int): height
+            x (int): lenght
+            y (int): height
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -14,10 +21,15 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ returns  the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ get width
+        Args:
+            value (int): value
+        """
         if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -26,10 +38,15 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """returns the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ get height
+        Args:
+            value (int): value
+        """
         if isinstance(value, int) is False:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -38,10 +55,15 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ returns lenght"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ get lenght
+        Args:
+            value (int): value
+        """
         if isinstance(value, int) is False:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -50,10 +72,15 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """returns height"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ get height
+        Args:
+            value (int): value
+        """
         if isinstance(value, int) is False:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -61,14 +88,17 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """returns area of the Rectangle"""
         return self.__width * self.__height
 
     def display(self):
+        """ displays rectangle"""
         for i in range(self.__height):
             for j in range(self.width):
                 print("#", end="")
             print()
 
     def __str__(self):
+        """ returns string with the rectangle arguments"""
         return "[Rectangle] (%s) %s/%s - %s/%s" % \
             (self.id, self.__x, self.__y, self.__width, self.__height)
