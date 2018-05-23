26a215ef775b69cbcee91f5c07dcda6a54b4d2be
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-23 06:36:01 +0000

    added method to_dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -143,3 +143,8 @@ class Rectangle(Base):
                     self.__x = kwargs[key]
                 if key == "y":
                     self.__y = kwargs[key]
+
+        def to_dictionary(self):
+            """returns the dictionary representation of a Rectangle """
+            return {'id': self.id, 'width': self.width,
+                    'height': self.height, 'x': self.x, 'y': self.y}
