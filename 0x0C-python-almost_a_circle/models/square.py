1a7c3e32f19d2158cc52f9a632e3566dc4810615
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-23 06:03:57 +0000

    class square updated

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Ititialize class Square
+        Args
+            width (int): widht
+            height (int): height
+            x (int): lenght
+            y (int): height
+            id (int): id
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ returns string with the square arguments"""
+        return "[Square] (%s) %s/%s - %s" % \
+            (self.id, self.x, self.y, self.width)
