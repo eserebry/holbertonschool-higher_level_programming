f7ee05d3794e7672fa0554372d8ab8be74151f47
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-24 01:24:19 +0000

    static method from_json_string added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,3 +44,17 @@ class Base:
                 savefile.append(cls.to_dictionary(list_objs[i]))
         with open(myfile, mode="w", encoding="utf-8") as new_file:
                 new_file.write(cls.to_json_string(savefile))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation
+        Args:
+            json_string (str): string representing a list of dictionaries
+        """
+        new_string = []
+        if json_string is None or len(json_string) is 0:
+            return new_string
+        else:
+            for i in range(len(json_string)):
+                new_string = json.loads(json_string)
+            return new_string
