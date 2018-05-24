786e19ab4552e9d62cb9759846cd02a19327daa9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-05-24 00:25:54 +0000

    returned value chnaged

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,8 +20,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or []:
-            return []
+        """ returns the JSON string representation of list_dictionaries
+        Args:
+            list_dictionaries (list): list of the dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
