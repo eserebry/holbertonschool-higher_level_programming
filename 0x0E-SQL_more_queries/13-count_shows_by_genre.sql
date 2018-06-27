bfd478b3b325364e6744e28e89951f80cfa6896a
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-27 23:39:01 +0000

    first commit

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,4 +6,4 @@ FROM tv_show_genres
      INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_shows DESC
+ORDER BY number_shows DESC;
