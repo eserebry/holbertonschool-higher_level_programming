f3e5f207e4f5e33e35d2f1c6e07c82a7716f212b
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-27 05:39:08 +0000

    unique statement moved

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- creates the database hbtn_0d_usa and the table cities on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-       UNIQUE(id),
        FOREIGN KEY (state_id) REFERENCES states(id));
