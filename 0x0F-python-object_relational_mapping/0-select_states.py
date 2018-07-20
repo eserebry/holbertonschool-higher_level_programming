d236d7e8dc415aae5be667fbb84a180f57c05786
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-07-20 05:56:25 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def list_states():
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id")
+    result = cur.fetchall()
+    for row in result:
+        print(row)
+    db.close()
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    list_states()
