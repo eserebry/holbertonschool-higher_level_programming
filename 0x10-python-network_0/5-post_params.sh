00635890af816bde4ec14bcddc728b593118cea9
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-01 06:07:09 +0000

    comment updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 curl -sd "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" $1
