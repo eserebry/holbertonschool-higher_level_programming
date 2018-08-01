0cdadd9a0d8bfb93a7601dc311057f2095788c97
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-01 06:45:38 +0000

    content-type added

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sdH $2 $1
+curl -s -d $2 -H 'Content-Type: application/json' $1
