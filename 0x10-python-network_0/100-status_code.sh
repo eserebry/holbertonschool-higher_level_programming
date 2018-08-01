f9656d7df19f8ecfa4001feee28097524a2f4e58
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-01 06:25:54 +0000

    solution found

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sI $1 | grep "HTTP/1.0" | cut -d " " -f2
+curl -so /dev/null -w "%{http_code}" $1
+
