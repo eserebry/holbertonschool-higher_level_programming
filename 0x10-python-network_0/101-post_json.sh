6e8da065ba739a3331781da2b84780f6d44bc7e0
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-01 06:48:31 +0000

    commet edited

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response.
+#  sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
 curl -s -d $2 -H 'Content-Type: application/json' $1
