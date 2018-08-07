2e53cd6af96c0053f75b43bee6718991b5041161
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-07 06:13:58 +0000

    first commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# takes in a URL, sends a request to the URL
+# and displays the body of the response (decoded in utf-8).
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+            utf_8 = the_page.decode('utf-8')
+        print(utf_8)
+    except urllib.error.URLError as e:
+        print(e.code)
