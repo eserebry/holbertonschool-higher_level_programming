7e816a55b905a1e2364750fc58b400c9589d6026
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-07 07:09:47 +0000

    firt commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# takes in a URL, sends a request to the URL
+# and displays the body of the response (decoded in utf-8).
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.text)
+    except Response.raise_for_status  as e:
+        print("Error code: {}".format(e.status_code))
