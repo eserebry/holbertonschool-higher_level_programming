10c0884ce953097450f8fc54c1f64d782e5d652d
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-08-11 02:57:29 +0000

    checking status code > 400

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,8 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
         print(r.text)
-    except Response.raise_for_status as e:
-        print("Error code: {}".format(e.status_code))
