82aee022e99425345c88dac7242dbef3219e02dd
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-05 04:07:42 +0000

    first commt

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    if (width <= 0 || height <= 0 || !width || !height) {
+      module.exports = class Rectangle {};
+    } else {
+      this.width = width;
+      this.height = height;
+    }
+  }
+  print () {
+    let x = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(x.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
