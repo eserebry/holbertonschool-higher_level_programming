706cdd32371ec5f2971e8f8277d5c95ceadb5fc8
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-08 03:31:19 +0000

    first commit

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#add_item").click(function(){
+	$("ul.my_list").append("<li>Item</li>");
+    });
+    $("#remove_item").click(function(){
+        $("li").last().remove();
+    });
+    $("#clear_list").click(function(){
+        $("ul").empty();
+    });
+});
